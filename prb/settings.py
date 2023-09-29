c66c443601ff5c7fab3bf3835abd84ef1c4707a1
praneeth-rdy <praneeth.kolanu.iitkgp@gmail.com>
2023-09-29 14:04:16 +0530

    Updated allowed hosts

--- prb/settings.py
+++ prb/settings.py
@@ -30,7 +30,7 @@ SECRET_KEY = env('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['127.0.0.1', 'praneethrdy.pythonanywhere.com']
+ALLOWED_HOSTS = ['127.0.0.1', 'praneethrdy.pythonanywhere.com', '13.233.158.42']
 
 
 # Application definition
